251a445362af30006b9a0d3475e778ec36ed839a
Kevin Mårtensson <kevinmartensson@gmail.com>
2017-06-30 19:37:24 +0200

    Add possibility to override with `ELECTRON_IS_DEV` environment variable (#7)

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 'use strict';
-module.exports = process.defaultApp || /[\\/]electron-prebuilt[\\/]/.test(process.execPath) || /[\\/]electron[\\/]/.test(process.execPath);
+
+const getFromEnv = () => {
+	if ('ELECTRON_IS_DEV' in process.env) {
+		return parseInt(process.env.ELECTRON_IS_DEV, 10) === 1;
+	}
+
+	return false;
+};
+
+module.exports = getFromEnv() || process.defaultApp || /[\\/]electron-prebuilt[\\/]/.test(process.execPath) || /[\\/]electron[\\/]/.test(process.execPath);
