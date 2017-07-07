c1908fb62b2dc71123fd7779004e8402e2dab307
Kevin Martensson <kevinmartensson@gmail.com>
2017-07-07 13:46:41 +0200

    Only match `electron` path inside `node_modules`
    Fixes #4.

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 const getFromEnv = parseInt(process.env.ELECTRON_IS_DEV, 10) === 1;
 const isEnvSet = 'ELECTRON_IS_DEV' in process.env;
 
-module.exports = isEnvSet ? getFromEnv : (process.defaultApp || /[\\/]electron-prebuilt[\\/]/.test(process.execPath) || /[\\/]electron[\\/]/.test(process.execPath));
+module.exports = isEnvSet ? getFromEnv : (process.defaultApp || /[\\/]electron-prebuilt[\\/]/.test(process.execPath) || /node_modules[\\/]electron[\\/]/.test(process.execPath));
