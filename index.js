ccc5acda0516eeff1f6a0195e73dd7e007782bfd
Kevin Mårtensson <kevinmartensson@gmail.com>
2017-07-07 13:42:32 +0200

    Make `ELECTRON_IS_DEV` override default behavior (#8)

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 'use strict';
+const getFromEnv = parseInt(process.env.ELECTRON_IS_DEV, 10) === 1;
+const isEnvSet = 'ELECTRON_IS_DEV' in process.env;
 
-const getFromEnv = () => {
-	if ('ELECTRON_IS_DEV' in process.env) {
-		return parseInt(process.env.ELECTRON_IS_DEV, 10) === 1;
-	}
-
-	return false;
-};
-
-module.exports = getFromEnv() || process.defaultApp || /[\\/]electron-prebuilt[\\/]/.test(process.execPath) || /[\\/]electron[\\/]/.test(process.execPath);
+module.exports = isEnvSet ? getFromEnv : (process.defaultApp || /[\\/]electron-prebuilt[\\/]/.test(process.execPath) || /[\\/]electron[\\/]/.test(process.execPath));
