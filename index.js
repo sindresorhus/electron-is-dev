db5377fa411083289599cfca8e2d9814b33dc611
popod <popod@me.com>
2018-09-19 20:06:51 +0200

    Fix undefined isPackaged bug in renderer process

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
-const {app} = require('electron');
+const electron = require('electron');
+
+const app = electron.app || electron.remote.app;
 
 const isEnvSet = 'ELECTRON_IS_DEV' in process.env;
 const getFromEnv = parseInt(process.env.ELECTRON_IS_DEV, 10) === 1;
