76713ecd34d327e375df3971ff500531178d49f2
Steven Roussey <42726156+stevenroussey-desiclabs@users.noreply.github.com>
2019-06-07 14:16:36 -0700

    Fix for isPackaged which is a function
    It was always returning true, since a function object is truthy.

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ const app = electron.app || electron.remote.app;
 const isEnvSet = 'ELECTRON_IS_DEV' in process.env;
 const getFromEnv = parseInt(process.env.ELECTRON_IS_DEV, 10) === 1;
 
-module.exports = isEnvSet ? getFromEnv : !app.isPackaged;
+module.exports = isEnvSet ? getFromEnv : !app.isPackaged();
