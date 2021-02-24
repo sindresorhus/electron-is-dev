7f71e302ba1f7f87a038d8c3d9f0f256ef11a00a
Remie <remie.smith@gmail.com>
2021-02-24 17:03:13 -0500

    Removes 'remote' module

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ if (typeof electron === 'string') {
 	throw new TypeError('Not running in an Electron environment!');
 }
 
-const app = electron.app || electron.remote.app;
-
 const isEnvSet = 'ELECTRON_IS_DEV' in process.env;
 const getFromEnv = parseInt(process.env.ELECTRON_IS_DEV, 10) === 1;
 
-module.exports = isEnvSet ? getFromEnv : !app.isPackaged;
+module.exports = isEnvSet ? getFromEnv : !electron.app.isPackaged;
