09a2c7f99667c551b39edbe4a4cb69b044bf99ce
Nick Webster <nick@nick.geek.nz>
2021-09-15 22:05:38 +1200

    Handle running in a remote situation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 const electron = require('electron');
+const remote = require('@electron/remote');
 
 if (typeof electron === 'string') {
 	throw new TypeError('Not running in an Electron environment!');
@@ -8,4 +9,4 @@ if (typeof electron === 'string') {
 const isEnvSet = 'ELECTRON_IS_DEV' in process.env;
 const getFromEnv = Number.parseInt(process.env.ELECTRON_IS_DEV, 10) === 1;
 
-module.exports = isEnvSet ? getFromEnv : !electron.app.isPackaged;
+module.exports = isEnvSet ? getFromEnv : !(electron.app || remote.app).isPackaged;
